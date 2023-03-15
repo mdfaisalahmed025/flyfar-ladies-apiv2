6acbf140da082574408de225400cdbe65dcdb575
mdfaisalahmed025 <71064436+mdfaisalahmed025@users.noreply.github.com>
2023-03-15 13:02:41 +0600

    fixed

--- src/tourpackage/tourpackage.controller.ts
+++ src/tourpackage/tourpackage.controller.ts
@@ -75,55 +75,25 @@ export class TourpackageController {
   }
 
   @Patch(':Id')
-  @UseInterceptors(
-    FilesInterceptor('updateimage',5,{
-      storage: diskStorage({
-        destination: './CoverImage',
-        filename: (req, image, callback) => {
-          // const uniqueSuffix = Date.now() + '-' +Math.round(Math.random()*1e9);
-          // const ext = extname(image.originalname)
-          const filename = `${image.originalname}`;
-          callback(null, filename);
-        },
-      }),
-    }),
-  )
- async update(
-  @UploadedFiles(
-    new ParseFilePipeBuilder()
-      .addFileTypeValidator({
-        fileType: /(jpg|jpeg|png|gif)$/,
-      })
-      .addMaxSizeValidator({
-        maxSize: 1024 * 1024 * 6,
-      })
-      .build({
-        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-      }),
-  )
-  files: Express.Multer.File[],
+ async update( 
   @Param('Id') Id: number,
   @Req() req: Request,
   @Res() res: Response,
   @Body() body) {
-
   const update = await this.TourpackageRepo.findOne({where:{Id}})
-    for (const file of files) {
-      update.ImageUrl = file.path
-      update.MainTitle = req.body.MainTitle
-      update.PkId= req.body.PkId
-      update.SubTitle =req.body.SubTitle
-      update.Price =req.body.Price
-      update.Location =req.body.Location
-      update.Availability =req.body.Availability
-      update.StartDate =req.body.StartDate
-      update.EndDate =req.body.EndDate
-      update.TripType =req.body.TripType
-      update.TotalDuration =req.body.TotalDuration
-      update.PackageOverview =req.body.PackageOverview
-      update.Showpackage =req.body.Showpackage
-      await this.TourpackageRepo.save({ ...update})
-    }
+  update.MainTitle = req.body.MainTitle
+  update.PkId= req.body.PkId
+  update.SubTitle =req.body.SubTitle
+  update.Price =req.body.Price
+  update.Location =req.body.Location
+  update.Availability =req.body.Availability
+  update.StartDate =req.body.StartDate
+  update.EndDate =req.body.EndDate
+  update.TripType =req.body.TripType
+  update.TotalDuration =req.body.TotalDuration
+  update.PackageOverview =req.body.PackageOverview
+  update.Showpackage =req.body.Showpackage
+  await this.TourpackageRepo.save({ ...update})
   return res.status(HttpStatus.OK).send({ status:"success", message:"Travel package updated succesfully" })
   }
 
