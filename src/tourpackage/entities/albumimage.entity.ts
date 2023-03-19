b3122a5dba4bbccb7a3dacd64186cd76c80f4a40
mdfaisalahmed025 <71064436+mdfaisalahmed025@users.noreply.github.com>
2023-03-19 16:00:19 +0600

    update default value

--- src/tourpackage/entities/albumimage.entity.ts
+++ src/tourpackage/entities/albumimage.entity.ts
@@ -8,14 +8,6 @@ export class AlbumImage{
    AlbumId: number;
    @Column({default:true})
    AlbumTitle: string;
-   @Column()
-   fieldname: string;
-   @Column()
-   originalname: string;
-   @Column()
-   destination: string;
-   @Column()
-   filename: string;
    @Column({default:true})
    albumImageUrl: string;
    @ManyToOne(() => Tourpackage, tourpackage=>tourpackage.albumImages)
